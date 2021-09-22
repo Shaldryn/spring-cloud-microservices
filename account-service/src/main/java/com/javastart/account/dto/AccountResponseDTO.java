89fb948a8b9ae311c156356370481a3a68fc11cb
Shaldryn <Shaldryn@yandex.ru>
2021-09-23 00:38:41 +0300

    add business logic account, bill services

---
+++ account-service/src/main/java/com/javastart/account/dto/AccountResponseDTO.java
@@ -0,0 +1,34 @@
+package com.javastart.account.dto;
+
+import com.javastart.account.entity.Account;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.time.OffsetDateTime;
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
+public class AccountResponseDTO {
+
+    private Long accountId;
+
+    private String name;
+
+    private String email;
+
+    private List<Long> bills;
+
+    private String phone;
+
+    private OffsetDateTime creationDate;
+
+    public AccountResponseDTO(Account account) {
+        accountId = account.getAccountId();
+        name = account.getName();
+        email = account.getEmail();
+        bills = account.getBills();
+        phone = account.getPhone();
+        creationDate = account.getCreationDate();
+    }
+}
